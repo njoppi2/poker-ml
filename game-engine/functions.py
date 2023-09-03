6e8f4b0c1e93065da17ac9780baf13e3fa6d227f
Nelson Joppi <njoppi2@gmail.com>
2023-09-03 00:30:24 -0300

    add reorder_list

---
+++ game-engine/functions.py
@@ -0,0 +1,14 @@
+import uuid
+import random
+import requests
+from typing import List, Tuple
+from player import Player
+
+def reorder_list(old_list, condition):
+    try:
+        index = next(i for i, item in enumerate(old_list) if condition(item))
+    except StopIteration:
+        # If the condition is not satisfied by any element, return the original list
+        return old_list
+
+    return old_list[index:] + old_list[:index]
