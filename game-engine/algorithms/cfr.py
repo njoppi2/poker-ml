2cc285756cd09ea79453bd2724054590ea6859cf
Nelson Joppi <njoppi2@gmail.com>
2023-10-22 17:58:20 -0300

    create kuhn simulator as a copy of kuhn3bet_mccfr, use snake_case in all files

--- game-engine/algorithms/cfr.py
+++ game-engine/algorithms/cfr.py
@@ -14,7 +14,7 @@ NUM_ACTIONS = len(Actions)
 class KuhnTrainer:
 
     def __init__(self):
-        self.nodeMap = {}
+        self.node_map = {}
         self.log_file = None
 
     def log(self, log_file):
@@ -32,37 +32,37 @@ class KuhnTrainer:
     
     class Node:
         def __init__(self):
-            self.infoSet = ""
-            self.regretSum = [0.0] * NUM_ACTIONS
+            self.info_set = ""
+            self.regret_sum = [0.0] * NUM_ACTIONS
             self.strategy = [0.0] * NUM_ACTIONS
-            self.strategySum = [0.0] * NUM_ACTIONS
+            self.strategy_sum = [0.0] * NUM_ACTIONS
 
-        def getStrategy(self, realizationWeight):
+        def get_strategy(self, realization_weight):
             """Turn sum of regrets into a probability distribution for actions."""
-            normalizingSum = sum(max(regret, 0) for regret in self.regretSum)
+            normalizing_sum = sum(max(regret, 0) for regret in self.regret_sum)
             for action in Actions:
-                if normalizingSum > 0:
-                    self.strategy[action.value] = max(self.regretSum[action.value], 0) / normalizingSum
+                if normalizing_sum > 0:
+                    self.strategy[action.value] = max(self.regret_sum[action.value], 0) / normalizing_sum
                 else:
                     self.strategy[action.value] = 1.0 / NUM_ACTIONS
-                self.strategySum[action.value] += realizationWeight * self.strategy[action.value]
+                self.strategy_sum[action.value] += realization_weight * self.strategy[action.value]
             return self.strategy
 
-        def getAverageStrategy(self):
-            avgStrategy = [0.0] * NUM_ACTIONS
-            normalizingSum = sum(self.strategySum)
+        def get_average_strategy(self):
+            avg_strategy = [0.0] * NUM_ACTIONS
+            normalizing_sum = sum(self.strategy_sum)
             for action in Actions:
-                if normalizingSum > 0:
-                    avgStrategy[action.value] = self.strategySum[action.value] / normalizingSum
+                if normalizing_sum > 0:
+                    avg_strategy[action.value] = self.strategy_sum[action.value] / normalizing_sum
                 else:
-                    avgStrategy[action.value] = 1.0 / NUM_ACTIONS
-            return avgStrategy
+                    avg_strategy[action.value] = 1.0 / NUM_ACTIONS
+            return avg_strategy
 
         def __str__(self):
-            return f"{self.infoSet}: {self.getAverageStrategy()}"
+            return f"{self.info_set}: {self.get_average_strategy()}"
         
         def __lt__(self, other):
-            return self.infoSet < other.infoSet
+            return self.info_set < other.info_set
 
 
     def train(self, iterations):
@@ -72,18 +72,18 @@ class KuhnTrainer:
             random.shuffle(cards)
             util += self.cfr(cards, "", 1, 1)
             
-            dict = ""
-            for n in self.nodeMap.values():
-                dict += str(n) + "\n"
+            dict_str = ""
+            for n in self.node_map.values():
+                dict_str += str(n) + "\n"
             sample_iteration = {
                 'index': i,
                 'avg_game_value': util / (i + 1),
-                'result_dict': dict  # Assuming self.nodeMap contains the result dictionary
+                'result_dict': dict_str  # Assuming self.node_map contains the result dictionary
             }
             self.logger.info('', extra=sample_iteration)
             
         print(f"Average game value: {util / iterations}")
-        for n in sorted(self.nodeMap.values()):
+        for n in sorted(self.node_map.values()):
             print(n)
 
     def cfr(self, cards, history, p0, p1):
@@ -92,44 +92,44 @@ class KuhnTrainer:
         opponent = 1 - player
 
         if plays > 1:
-            terminalPass = history[-1] == 'p'
-            doubleBet = history[-2:] == 'bb'
-            isPlayerCardHigher = cards[player] > cards[opponent]
+            terminal_pass = history[-1] == 'p'
+            double_bet = history[-2:] == 'bb'
+            is_player_card_higher = cards[player] > cards[opponent]
 
-            if terminalPass:
+            if terminal_pass:
                 if history == "pp":
-                    return 1 if isPlayerCardHigher else -1          #determina a recompensa
+                    return 1 if is_player_card_higher else -1          #determina a recompensa
                 else:
                     return 1
-            elif doubleBet:
-                return 2 if isPlayerCardHigher else -2              #determina a recompensa
+            elif double_bet:
+                return 2 if is_player_card_higher else -2              #determina a recompensa
 
-        infoSet = str(cards[player]) + history                      #determina o infoset olhando a carta do jogador atual e o histórico
-        node = self.nodeMap.get(infoSet)                            #tenta pegar o nodo do infoset correspondente
+        info_set = str(cards[player]) + history                      #determina o infoset olhando a carta do jogador atual e o histórico
+        node = self.node_map.get(info_set)                            #tenta pegar o nodo do infoset correspondente
 
         if node is None:                                            #se não existir, cria um novo nodo
             node = self.Node()
-            node.infoSet = infoSet
-            self.nodeMap[infoSet] = node
-            #print(infoSet)
+            node.info_set = info_set
+            self.node_map[info_set] = node
+            #print(info_set)
 
-        strategy = node.getStrategy(p0 if player == 0 else p1)      #pega a estratégia do nodo. ******************entender melhor******************
+        strategy = node.get_strategy(p0 if player == 0 else p1)      #pega a estratégia do nodo. ******************entender melhor******************
         util = [0.0] * NUM_ACTIONS
-        nodeUtil = 0
+        node_util = 0
 
         for action in Actions:                                      #itera entre todas as acoes possiveis, calculando a utilidade esperada. Possivelmente eh aqui que implemente o MCCFR
-            nextHistory = history + ('p' if action == Actions.PASS else 'b')
+            next_history = history + ('p' if action == Actions.PASS else 'b')
             if player == 0:
-                util[action.value] = -self.cfr(cards, nextHistory, p0 * strategy[action.value], p1) #aqui implementa a recursao, percorrendo os nodos e calculando a utilidade esperada
+                util[action.value] = -self.cfr(cards, next_history, p0 * strategy[action.value], p1) #aqui implementa a recursao, percorrendo os nodos e calculando a utilidade esperada
             else:
-                util[action.value] = -self.cfr(cards, nextHistory, p0, p1 * strategy[action.value])
-            nodeUtil += strategy[action.value] * util[action.value]
+                util[action.value] = -self.cfr(cards, next_history, p0, p1 * strategy[action.value])
+            node_util += strategy[action.value] * util[action.value]
 
         for action in Actions:
-            regret = util[action.value] - nodeUtil
-            node.regretSum[action.value] += (p1 if player == 0 else p0) * regret
+            regret = util[action.value] - node_util
+            node.regret_sum[action.value] += (p1 if player == 0 else p0) * regret
 
-        return nodeUtil
+        return node_util
 
 if __name__ == "__main__":
     iterations = 1000
