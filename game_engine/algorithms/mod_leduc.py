025d2afbfc818d8312f885be9471be0fcc913eeb
Nelson Joppi <njoppi2@gmail.com>
2023-10-24 05:20:09 -0300

    make sure mod_leduc and kuhn3bet are equal

--- game_engine/algorithms/mod_leduc.py
+++ game_engine/algorithms/mod_leduc.py
@@ -7,8 +7,6 @@ from functions import color_print, create_file, float_to_custom_string
 import time
 import json
 import multiprocessing
-from game_engine.player import Player
-
 
 
 random.seed(42)
@@ -20,43 +18,31 @@ current_file_name = os.path.splitext(current_file_with_extension)[0]
 
 iterations = 1000
 use_3bet = True
-algorithm = 'cfr'
-cards = [1, 1, 2, 2, 3, 3]
+algorithm = 'mccfr'
+cards = [1, 2, 3]#, 4, 5, 6, 7, 8, 9]
 exploring_phase = 0.0
 
 
 if use_3bet:
     class Actions(Enum):
         PASS = 0
-        BET4 = 1
-        BET3 = 2
-        BET2 = 3
-        BET1 = 4
-    action_symbol = ['p', '4', '3', '2', '1']
+        BET1 = 1
+        BET2 = 2
+    action_symbol = ['p', 'b', 'B']
 else:
     class Actions(Enum):
         PASS = 0
         BET1 = 1
     action_symbol = ['p','b']
 
-class ModLeducTrainer:
+class KuhnTrainer:
     """
-       The AI's wil play a version of Leduc Poker with 5 possible actions: pass, bet 1, bet 2, bet 3, and bet 4. 
-       Each player has 4 coins, if someone does BET4, they're all-in.
-       There are 2 rounds, and at the beggining of the second round, a public card is revealed.
-       Possible final histories for the round 1: 
-        2+0 pot: pp
-        2+1 pot: p1p, 1p
-        2+2 pot: p11, p2p, 2p
-        2+3 pot: p12p, p3p, 12p, 3p
-        2+4 pot: p121, p13p, p22, p4p, 121, 13p, 22, 4p
-        2+5 pot: p122p, p14p, p23p, p5p, 122p, 14p, 23p, 5p
-        2+6 pot: p1221, p123p, p132, p15p, p231, p24p, p33, p6p, 1221, 123p, 132, 15p, 231, 24p, 33, 6p
-        2+7 pot: p1222p, p124p, p133p, p141, p16p, ....
-        2+8 pot: ......
-
-        p122K111
-        flop: K
+       The AI's wil play a version of Kuhn Poker with 3 possible actions: pass, bet 1 and bet 2. 
+       Each player has 2 coins, if someone does BET2, they're all-in.
+       Possible final histories: 
+        pp, pbp, pbb, pbBp, pbBb, pBp, pBb, pBB,
+        bp,       bb,  bBp,  bBb,
+        Bp,                                 BB 
     """
     # How can we handle situations where not all actions are alowed?
 
@@ -82,7 +68,7 @@ class ModLeducTrainer:
         """ A node is an information set, which is the cards of the player and the history of the game."""
         def __init__(self, info_set, actions=Actions):
 
-            self.actions = actions
+            self.actions = list(actions)
             self.num_actions = len(actions)
             self.info_set = info_set
             # The regret and strategies of a node refer to the last action taken to reach the node
@@ -93,12 +79,12 @@ class ModLeducTrainer:
         def get_strategy(self, realization_weight):
             """Turn sum of regrets into a probability distribution for actions."""
             normalizing_sum = sum(max(regret, 0) for regret in self.regret_sum)
-            for action in self.actions:
+            for i in range(len(self.actions)):
                 if normalizing_sum > 0:
-                    self.strategy[action.value] = max(self.regret_sum[action.value], 0) / normalizing_sum
+                    self.strategy[i] = max(self.regret_sum[i], 0) / normalizing_sum
                 else:
-                    self.strategy[action.value] = 1.0 / self.num_actions
-                self.strategy_sum[action.value] += realization_weight * self.strategy[action.value]
+                    self.strategy[i] = 1.0 / self.num_actions
+                self.strategy_sum[i] += realization_weight * self.strategy[i]
             return self.strategy
         
         def get_action(self, strategy):
@@ -106,21 +92,21 @@ class ModLeducTrainer:
             r = random.random()
             cumulative_probability = 0
             
-            for action in self.actions:
-                cumulative_probability += strategy[action.value]
+            for i in range(len(self.actions)):
+                cumulative_probability += strategy[i]
                 if r < cumulative_probability:
-                    return action
+                    return self.actions[i]
             
             raise Exception("No action taken for r: " + str(r) + " and cumulativeProbability: " + str(cumulative_probability) + " and strategy: " + str(strategy))
 
         def get_average_strategy(self):
             avg_strategy = [0.0] * self.num_actions
             normalizing_sum = sum(self.strategy_sum)
-            for action in self.actions:
+            for i in range(len(self.actions)):
                 if normalizing_sum > 0:
-                    avg_strategy[action.value] = self.strategy_sum[action.value] / normalizing_sum
+                    avg_strategy[i] = self.strategy_sum[i] / normalizing_sum
                 else:
-                    avg_strategy[action.value] = 1.0 / self.num_actions
+                    avg_strategy[i] = 1.0 / self.num_actions
             return avg_strategy
 
         def __str__(self):
@@ -142,9 +128,11 @@ class ModLeducTrainer:
     def get_possible_actions(self, history, cards, player, opponent):
         """Returns the reward if it's a terminal node, or the possible actions if it's not."""
         is_player_card_higher = cards[player] > cards[opponent]
-        
+
         if len(history) == 0:
-            return Actions, None
+            return list(Actions), None
+        elif history[-3:] == "bBp":
+            return None, 2
         elif len(history) >= 2:
             if history[-2:] == "pp":
                 return None, 1 if is_player_card_higher else -1
@@ -156,13 +144,17 @@ class ModLeducTrainer:
                 return None, 3 if is_player_card_higher else -3
             elif history[-3:] == "bBb":
                 return None, 3 if is_player_card_higher else -3
-        
+        if history[-2:] == 'bB':
+            possible_actions1 = list(Actions)
+            possible_actions1.remove(Actions(Actions.BET2))  # Remove the first action from the list
+            return possible_actions1, None
+
         if history[-1] == 'B':
             possible_actions = list(Actions)
             possible_actions.remove(Actions(Actions.BET1))  # Remove the first action from the list
             return possible_actions, None
         else:
-            return Actions, None
+            return list(Actions), None
 
         return Actions, None
         # raise Exception("Action or reward not found for history: " + history)
@@ -214,7 +206,7 @@ class ModLeducTrainer:
         elapsed_time = end_time - start_time
         print(f"{algorithm} took {elapsed_time} seconds to run.")
         json_name = f'{"3bet" if use_3bet else "2bet"}-{algorithm}-{len(cards)}cards-EP{float_to_custom_string(exploring_phase)}.json'
-        final_strategy_path = f'../analysis/blueprints/kuhn-{json_name}'
+        final_strategy_path = f'../analysis/blueprints/kuhntest-{json_name}'
         create_file(final_strategy_path)
 
         node_dict = {}
@@ -230,26 +222,26 @@ class ModLeducTrainer:
         return self.node_map.setdefault(info_set, self.Node(info_set, possible_actions))
 
 
-    def play_mccfr(self, cards, history, p0, p1, strategy, player, action, is_exploring_phase, alternative_play=None):
+    def play_mccfr(self, cards, history, p0, p1, strategy, player, action, action_index, is_exploring_phase, alternative_play=None):
         action_char = action_symbol[action.value]
         next_history = history + action_char
         node_action_utility = 0
         # node_action_utility receives a negative values because we are alternating between players,
         # and in the Leduc Poker game, the reward for a player is the opposite of the other player's reward
         if player == 0:
-            node_action_utility = -self.mccfr(cards, next_history, p0 * strategy[action.value], p1, is_exploring_phase, alternative_play)
+            node_action_utility = -self.mccfr(cards, next_history, p0 * strategy[action_index], p1, is_exploring_phase, alternative_play)
         else:
-            node_action_utility = -self.mccfr(cards, next_history, p0, p1 * strategy[action.value], is_exploring_phase, alternative_play)
+            node_action_utility = -self.mccfr(cards, next_history, p0, p1 * strategy[action_index], is_exploring_phase, alternative_play)
 
         return node_action_utility
 
-    def play_cfr(self, cards, history, p0, p1, strategy, player, action, is_exploring_phase):
+    def play_cfr(self, cards, history, p0, p1, strategy, player, action, action_index, is_exploring_phase):
         action_char = action_symbol[action.value]
         next_history = history + action_char
         if player == 0:
-            return -self.cfr(cards, next_history, p0 * strategy[action.value], p1, is_exploring_phase)
+            return -self.cfr(cards, next_history, p0 * strategy[action_index], p1, is_exploring_phase)
         else:
-            return -self.cfr(cards, next_history, p0, p1 * strategy[action.value], is_exploring_phase)
+            return -self.cfr(cards, next_history, p0, p1 * strategy[action_index], is_exploring_phase)
 
     def mccfr(self, cards, history, p0, p1, is_exploring_phase, alternative_play=None):
         # On the first iteration, the history is empty, so the first player starts
@@ -274,21 +266,24 @@ class ModLeducTrainer:
         other_actions = list(possible_actions)  # Get a list of actions in the order of the enum
         chosen_action = node.get_action(strategy)
         other_actions.remove(Actions(chosen_action))  # Remove the first action from the list
+        action_index = node.actions.index(chosen_action)
 
         # Play chosen action according to the strategy
-        node_chosen_action_utility = self.play_mccfr(cards, history, p0, p1, strategy, player, chosen_action, is_exploring_phase, alternative_play)
-        node_actions_utilities[chosen_action.value] = node_chosen_action_utility
+        node_chosen_action_utility = self.play_mccfr(cards, history, p0, p1, strategy, player, chosen_action, action_index, is_exploring_phase, alternative_play)
+        node_actions_utilities[action_index] = node_chosen_action_utility
 
         # Play for other actions
         if alternative_play is None or alternative_play == player:
             for action in other_actions:
+                action_index = node.actions.index(action)
                 # passar um parametro para mccfr dizendo que se é jogada alternativa, e de quê jogador, se for do jogador 1, ai não tem for na jogada do jogador 0
-                node_action_utility = self.play_mccfr(cards, history, p0, p1, strategy, player, action, is_exploring_phase, alternative_play=player)
-                node_actions_utilities[action.value] = node_action_utility
+                node_action_utility = self.play_mccfr(cards, history, p0, p1, strategy, player, action, action_index, is_exploring_phase, alternative_play=player)
+                node_actions_utilities[action_index] = node_action_utility
 
-        for action in possible_actions:
-            regret = node_actions_utilities[action.value] - node_chosen_action_utility
-            node.regret_sum[action.value] += (p1 if player == 0 else p0) * regret
+            for action in possible_actions:
+                action_index = node.actions.index(action)
+                regret = node_actions_utilities[action_index] - node_chosen_action_utility
+                node.regret_sum[action_index] += (p1 if player == 0 else p0) * regret
 
         return node_chosen_action_utility
     
@@ -315,18 +310,20 @@ class ModLeducTrainer:
         node_util = 0
 
         for action in possible_actions:
-            node_action_utility = self.play_cfr(cards, history, p0, p1, strategy, player, action, is_exploring_phase)
-            node_actions_utilities[action.value] = node_action_utility
-            node_util += strategy[action.value] * node_action_utility
+            action_index = node.actions.index(action)
+            node_action_utility = self.play_cfr(cards, history, p0, p1, strategy, player, action, action_index, is_exploring_phase)
+            node_actions_utilities[action_index] = node_action_utility
+            node_util += strategy[action_index] * node_action_utility
 
         for action in possible_actions:
-            regret = node_actions_utilities[action.value] - node_util
-            node.regret_sum[action.value] += (p1 if player == 0 else p0) * regret
+            action_index = node.actions.index(action)
+            regret = node_actions_utilities[action_index] - node_util
+            node.regret_sum[action_index] += (p1 if player == 0 else p0) * regret
 
         return node_util
 
 
 if __name__ == "__main__":
-    trainer = ModLeducTrainer()
+    trainer = KuhnTrainer()
     trainer.log(f'../analysis/logs/{current_file_name}_{algorithm}.log')
     trainer.train(iterations)
