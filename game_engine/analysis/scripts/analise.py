3e25224231daef8ec7a7c864e53c70286f247e26
Igor May Wensing <igormaywensing@gmail.com>
2023-11-02 20:20:51 -0300

    add plotting and statistical insight

--- game_engine/analysis/scripts/analise.py
+++ game_engine/analysis/scripts/analise.py
@@ -1,4 +1,45 @@
 import sys
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+def plot_statistics(rewards, stats):
+    # Convert rewards to a pandas Series for cumulative sum
+    rewards_series = pd.Series(rewards).cumsum()
+
+    # Plot the bankroll over time
+    plt.figure(figsize=(12, 6))
+    rewards_series.plot(title='Bankroll Over Time')
+    plt.xlabel('Game Number')
+    plt.ylabel('Bankroll')
+    plt.grid(True)
+    plt.show()
+
+    # Plot the win/draw/loss distribution
+    labels = 'Wins', 'Draws', 'Losses'
+    sizes = [stats['win_rate'], stats['draw_rate'], stats['loss_rate']]
+    colors = ['green', 'gold', 'red']
+    explode = (0.1, 0, 0)  # explode 1st slice (Wins)
+
+    plt.figure(figsize=(6, 6))
+    plt.pie(sizes, explode=explode, labels=labels, colors=colors,
+            autopct='%1.1f%%', shadow=True, startangle=140)
+    plt.axis('equal')
+    plt.title('Game Outcome Distribution')
+    plt.show()
+
+    # Plot win/loss streaks
+    labels = ['Longest Win Streak', 'Longest Loss Streak', 'Max Streak']
+    values = [stats['longest_win_streak'], stats['longest_loss_streak'], stats['max_streak']]
+
+    plt.figure(figsize=(8, 5))
+    plt.bar(labels, values, color=['blue', 'red', 'purple'])
+    plt.title('Win and Loss Streaks')
+    plt.ylabel('Number of Games')
+    for i, v in enumerate(values):
+        plt.text(i, v + 0.25, str(v), color='black', ha='center')
+    plt.tight_layout()
+    plt.show()
 
 def analyze_log_file(log_file_path):
     # Vetores para armazenar as partidas ganhas, empatadas e perdidas pelo TCC_AI
@@ -35,16 +76,57 @@ def analyze_log_file(log_file_path):
                 else:
                     tcc_ai_losses.append(line)
 
-    # Retornar os resultados
-    print(rewards)
-    return tcc_ai_wins, tcc_ai_draws, tcc_ai_losses
+    # Calculate statistics
+    total_games = len(tcc_ai_wins) + len(tcc_ai_draws) + len(tcc_ai_losses)
+    win_rate = len(tcc_ai_wins) / total_games
+    draw_rate = len(tcc_ai_draws) / total_games
+    loss_rate = len(tcc_ai_losses) / total_games
+    average_win_margin = np.mean([gain for gain in rewards if gain > 0]) if tcc_ai_wins else 0
+    average_loss_margin = np.mean([abs(gain) for gain in rewards if gain < 0]) if tcc_ai_losses else 0
+    std_deviation = np.std(rewards)
+    
+    # Calculate win/loss streaks
+    current_streak = max_streak = win_streak = loss_streak = 0
+    for gain in rewards:
+        if gain > 0:
+            if current_streak >= 0:
+                current_streak += 1
+            else:
+                current_streak = 1
+            win_streak = max(win_streak, current_streak)
+        elif gain < 0:
+            if current_streak <= 0:
+                current_streak -= 1
+            else:
+                current_streak = -1
+            loss_streak = max(loss_streak, -current_streak)
+        else:
+            current_streak = 0
+        max_streak = max(max_streak, abs(current_streak))
+
+    stats = {
+        'total_games': total_games,
+        'win_rate': win_rate,
+        'draw_rate': draw_rate,
+        'loss_rate': loss_rate,
+        'average_win_margin': average_win_margin,
+        'average_loss_margin': average_loss_margin,
+        'std_deviation': std_deviation,
+        'longest_win_streak': win_streak,
+        'longest_loss_streak': loss_streak,
+        'max_streak': max_streak
+    }
+
+    return tcc_ai_wins, tcc_ai_draws, tcc_ai_losses, rewards, stats
 
 def main(log_file_path):
-    wins, draws, losses = analyze_log_file(log_file_path)
-    # Imprimir os resultados
-    print("\nPartidas ganhas pelo TCC_AI:", wins[0])
-    print("\nPartidas empatadas pelo TCC_AI:", draws[0])
-    print("\nPartidas perdidas pelo TCC_AI:", losses[0])
+    wins, draws, losses, rewards, stats = analyze_log_file(log_file_path)
+    print("\nStatistics:")
+    for stat, value in stats.items():
+        print(f"{stat.replace('_', ' ').title()}: {value}")
+    plot_statistics(rewards, stats)
+
+
 
 if __name__ == "__main__":
     log_file_name = "important_logs/4kk_noParamenters_10kgames.log"
