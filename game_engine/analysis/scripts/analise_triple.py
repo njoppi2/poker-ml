13af72b997ebfbcd484c1ec7721971196b197b61
Igor May Wensing <igormaywensing@gmail.com>
2023-11-07 18:05:10 -0300

    Fix histogram plotting issue and add new functions for hand analysis

--- game_engine/analysis/scripts/analise_triple.py
+++ game_engine/analysis/scripts/analise_triple.py
@@ -52,104 +52,33 @@ def plot_gaussian(rewards):
 
     plt.show()
 
-def plot_statistics(rewards, stats):
-    # Convert rewards to a pandas Series for cumulative sum
-    rewards_series = pd.Series(rewards).cumsum()
+def plot_statistics(rewards1, rewards2, rewards3):
+    # Convert rewards to pandas Series for cumulative sum
+    rewards_series1 = pd.Series(rewards1).cumsum()
+    rewards_series2 = pd.Series(rewards2).cumsum()
+    rewards_series3 = pd.Series(rewards3).cumsum()
 
-    # Plot the bankroll over time
+
+    # Plot the bankroll over time for both series
     plt.figure(figsize=(12, 6))
-    rewards_series.plot(title='Bankroll Over Time')
+
+    # Plot each series on the same figure
+    rewards_series1.plot(label='Rewards 1', color='blue')
+    rewards_series2.plot(label='Rewards 2', color='orange')
+    rewards_series3.plot(label='Rewards 3', color='green')
+
+
+    # Title and labels
+    plt.title('Bankroll Over Time')
     plt.xlabel('Game Number')
     plt.ylabel('Bankroll')
+
+    # Show grid and legend
     plt.grid(True)
+    plt.legend()
+
+    # Display the plot
     plt.show()
-
-    # Plot the win/draw/loss distribution
-    labels = 'Wins', 'Draws', 'Losses'
-    sizes = [stats['win_rate'], stats['draw_rate'], stats['loss_rate']]
-    colors = ['green', 'gold', 'red']
-    explode = (0.1, 0, 0)  # explode 1st slice (Wins)
-
-    plt.figure(figsize=(6, 6))
-    plt.pie(sizes, explode=explode, labels=labels, colors=colors,
-            autopct='%1.1f%%', shadow=True, startangle=140)
-    plt.axis('equal')
-    plt.title('Game Outcome Distribution')
-    plt.show()
-
-def is_winning_hand(line):
-    CARDS_RANK = {'Q': 0, 'K': 1, 'A': 2, 'Pair': 3}
-    parts = line.split(':')
-    cards = parts[3].split('/')
-    if int(parts[1]) % 2 == 0:
-        my_card = cards[0].split('|')[1]
-        op_card = cards[0].split('|')[0]
-        my_card = ''.join(filter(str.isupper, my_card))
-        op_card = ''.join(filter(str.isupper, op_card))
-
-    else:
-        my_card = cards[0].split('|')[0]
-        op_card = cards[0].split('|')[1]
-        my_card = ''.join(filter(str.isupper, my_card))
-        op_card = ''.join(filter(str.isupper, op_card))
-
-    if len(cards) > 1:
-        cards[1] = ''.join(filter(str.isupper, cards[1]))
-
-    if (len(cards) > 1 and my_card == cards[1]):
-        my_rank = 3
-    else:
-        my_rank = CARDS_RANK[my_card]
-
-    if (len(cards) > 1 and op_card == cards[1]):
-        op_rank = 3
-    else:
-        op_rank = CARDS_RANK[op_card]
-
-    if (my_rank > op_rank):
-        return True
-    else:
-        return False
-
-def is_losing_hand(line):
-    cards_rank = {'Q': 0, 'K': 1, 'A': 2, 'Pair': 3}
-    parts = line.split(':')
-    cards = parts[3].split('/')
-    if int(parts[1]) % 2 == 0:
-        my_card = cards[0].split('|')[1]
-        op_card = cards[0].split('|')[0]
-        my_card = ''.join(filter(str.isupper, my_card))
-        op_card = ''.join(filter(str.isupper, op_card))
-    else:
-        my_card = cards[0].split('|')[0]
-        op_card = cards[0].split('|')[1]
-        my_card = ''.join(filter(str.isupper, my_card))
-        op_card = ''.join(filter(str.isupper, op_card))
-    
-    if len(cards) > 1:
-        cards[1] = ''.join(filter(str.isupper, cards[1]))
-
-    if (len(cards) > 1 and my_card == cards[1]):
-        my_rank = 3
-    else:
-        my_rank = cards_rank[my_card]
-
-    if (len(cards) > 1 and op_card == cards[1]):
-        op_rank = 3
-    else:
-        op_rank = cards_rank[op_card]
-
-    if (my_rank < op_rank):
-        return True
-    else:
-        return False        
-
-def print_matchlist_analysis(matchlist):
-    bad_hands = [match for match in matchlist if is_losing_hand(match)]
-    good_hands = [match for match in matchlist if is_winning_hand(match)]
-    print("Should have lost: ", len(bad_hands)/len(matchlist))
-    print("Should have won: ", len(good_hands)/len(matchlist))
-    print("Should have draw: ", 1 - (len(bad_hands)/len(matchlist) + len(good_hands)/len(matchlist)))
 
 def get_hand_quality_proportion(matchlist):
     CARDS_RANK = {'Q': 0, 'K': 1, 'A': 2, 'Pair': 3}
@@ -161,24 +90,34 @@ def get_hand_quality_proportion(matchlist):
         parts = line.split(':')
         cards = parts[3].split('/')
 
-        p1_card = ''.join(filter(str.isupper, cards[0].split('|')[0]))
-        p2_card = ''.join(filter(str.isupper, cards[0].split('|')[1]))
+        if int(parts[1]) % 2 == 0:
+            p1_card = cards[0].split('|')[0]
+            p2_card = cards[0].split('|')[1]
+        else:
+            p1_card = cards[0].split('|')[1]
+            p2_card = cards[0].split('|')[0]
 
-        p1_rank = CARDS_RANK[p1_card]
-        p2_rank = CARDS_RANK[p2_card]
+        p1_card = ''.join(filter(str.isupper, p1_card))
+        p2_card = ''.join(filter(str.isupper, p2_card))
 
-        # Check for the common card and adjust the rank if there's a pair
-        if len(cards) > 1 and cards[1].strip():
+        if len(cards) > 1:
             common_card = ''.join(filter(str.isupper, cards[1]))
-            if p1_card == common_card:
-                p1_rank = CARDS_RANK['Pair']
-            if p2_card == common_card:
-                p2_rank = CARDS_RANK['Pair']
+            if (p1_card == common_card):
+                p1_rank = 3
+            else:
+                p1_rank = CARDS_RANK[p1_card]
 
-        # Compare the ranks to determine win, draw, or loss
-        if p1_rank > p2_rank:
+            if (p2_card == common_card):
+                p2_rank = 3
+            else:
+                p2_rank = CARDS_RANK[p2_card]
+        else:
+            p1_rank = CARDS_RANK[p1_card]
+            p2_rank = CARDS_RANK[p2_card]
+
+        if (p1_rank > p2_rank):
             p1_should_win.append(line)
-        elif p1_rank == p2_rank:
+        elif (p1_rank == p2_rank):
             p1_should_draw.append(line)
         else:
             p1_should_lose.append(line)
@@ -187,51 +126,56 @@ def get_hand_quality_proportion(matchlist):
 
 def analyze_log_file(log_file_path):
     # Vetores para armazenar as partidas ganhas, empatadas e perdidas pelo TCC_AI
-    tcc_ai_wins = []
-    tcc_ai_draws = []
-    tcc_ai_losses = []
-    rewards = []
+    matches = []
+    p1_wins = []
+    p1_draws = []
+    p1_losses = []
+    p1_rewards = []
 
-
+    #count_games = 0
     # Abrir o arquivo e ler linha por linha
     with open(log_file_path, 'r') as file:
         for line in file:
+            # if count_games == 1000:
+            #     break
             # Verificar se a linha começa com "STATE:"
             if line.startswith("STATE:"):
+                #count_games += 1
                 # Separar os componentes da linha
                 parts = line.split(':')
 
                 # Extrair o ganho do jogador TCC_AI, que pode estar na quinta ou sexta posição da linha,
                 # dependendo de quem começou jogando a partida
                 if int(parts[1]) % 2 == 0:
-                    tcc_ai_gain = int(parts[4].split('|')[1])
+                    p1_gain = int(parts[4].split('|')[1])
 
                 else:
-                    tcc_ai_gain = int(parts[4].split('|')[0])
+                    p1_gain = int(parts[4].split('|')[0])
 
                 # Adicionar o ganho do jogador TCC_AI ao vetor de recompensas
                 if is_inverted_seat:
-                    tcc_ai_gain = -int(tcc_ai_gain)
+                    p1_gain = -int(p1_gain)
                 else:
-                    tcc_ai_gain = int(tcc_ai_gain)
-                rewards.append(int(tcc_ai_gain))
+                    p1_gain = int(p1_gain)
+                p1_rewards.append(int(p1_gain))
 
                 # Classificar a partida como ganha, empatada ou perdida
-                if tcc_ai_gain > 0:
-                    tcc_ai_wins.append(line)
-                elif tcc_ai_gain == 0:
-                    tcc_ai_draws.append(line)
+                matches.append(line)
+                if p1_gain > 0:
+                    p1_wins.append(line)
+                elif p1_gain == 0:
+                    p1_draws.append(line)
                 else:
-                    tcc_ai_losses.append(line)
+                    p1_losses.append(line)
 
     # Calculate statistics
-    total_games = len(tcc_ai_wins) + len(tcc_ai_draws) + len(tcc_ai_losses)
-    win_rate = len(tcc_ai_wins) / total_games
-    draw_rate = len(tcc_ai_draws) / total_games
-    loss_rate = len(tcc_ai_losses) / total_games
-    average_win_margin = np.mean([gain for gain in rewards if gain > 0]) if tcc_ai_wins else 0
-    average_loss_margin = np.mean([abs(gain) for gain in rewards if gain < 0]) if tcc_ai_losses else 0
-    std_deviation = np.std(rewards)
+    total_games = len(p1_wins) + len(p1_draws) + len(p1_losses)
+    win_rate = len(p1_wins) / total_games
+    draw_rate = len(p1_draws) / total_games
+    loss_rate = len(p1_losses) / total_games
+    average_win_margin = np.mean([gain for gain in p1_rewards if gain > 0]) if p1_wins else 0
+    average_loss_margin = np.mean([abs(gain) for gain in p1_rewards if gain < 0]) if p1_losses else 0
+    std_deviation = np.std(p1_rewards)
 
     stats = {
         'total_games': total_games,
@@ -244,34 +188,36 @@ def analyze_log_file(log_file_path):
         'expected_value': win_rate * average_win_margin + loss_rate * average_loss_margin
     }
 
-    return tcc_ai_wins, tcc_ai_draws, tcc_ai_losses, rewards, stats
+    return matches, p1_wins, p1_draws, p1_losses, p1_rewards, stats
 
 def analyze_directory(directory_path):
     # Lists to store the results from all files
-    all_tcc_ai_wins = []
-    all_tcc_ai_draws = []
-    all_tcc_ai_losses = []
-    all_rewards = []
+    all_matches = []
+    all_p1_wins = []
+    all_p1_draws = []
+    all_p1_losses = []
+    all_p1_rewards = []
 
     # Loop through each file in the directory
     for filename in os.listdir(directory_path):
         if filename.endswith('.log'):  # Check if the file is a log file
             log_file_path = os.path.join(directory_path, filename)
             # Analyze the log file and accumulate the results
-            tcc_ai_wins, tcc_ai_draws, tcc_ai_losses, rewards, _ = analyze_log_file(log_file_path)
-            all_tcc_ai_wins.extend(tcc_ai_wins)
-            all_tcc_ai_draws.extend(tcc_ai_draws)
-            all_tcc_ai_losses.extend(tcc_ai_losses)
-            all_rewards.extend(rewards)
+            matches, p1_wins, p1_draws, p1_losses, p1_rewards, _ = analyze_log_file(log_file_path)
+            all_matches.extend(matches)
+            all_p1_wins.extend(p1_wins)
+            all_p1_draws.extend(p1_draws)
+            all_p1_losses.extend(p1_losses)
+            all_p1_rewards.extend(p1_rewards)
 
     # Calculate statistics for all files
-    total_games = len(all_tcc_ai_wins) + len(all_tcc_ai_draws) + len(all_tcc_ai_losses)
-    win_rate = len(all_tcc_ai_wins) / total_games
-    draw_rate = len(all_tcc_ai_draws) / total_games
-    loss_rate = len(all_tcc_ai_losses) / total_games
-    average_win_margin = np.mean([gain for gain in all_rewards if gain > 0]) if all_tcc_ai_wins else 0
-    average_loss_margin = np.mean([abs(gain) for gain in all_rewards if gain < 0]) if all_tcc_ai_losses else 0
-    std_deviation = np.std(all_rewards)
+    total_games = len(all_p1_wins) + len(all_p1_draws) + len(all_p1_losses)
+    win_rate = len(all_p1_wins) / total_games
+    draw_rate = len(all_p1_draws) / total_games
+    loss_rate = len(all_p1_losses) / total_games
+    average_win_margin = np.mean([gain for gain in all_p1_rewards if gain > 0]) if all_p1_wins else 0
+    average_loss_margin = np.mean([abs(gain) for gain in all_p1_rewards if gain < 0]) if all_p1_losses else 0
+    std_deviation = np.std(all_p1_rewards)
 
     stats = {
         'total_games': total_games,
@@ -284,7 +230,7 @@ def analyze_directory(directory_path):
         'expected_value': win_rate * average_win_margin - loss_rate * average_loss_margin
     }
 
-    return all_tcc_ai_wins, all_tcc_ai_draws, all_tcc_ai_losses, all_rewards, stats
+    return all_matches, all_p1_wins, all_p1_draws, all_p1_losses, all_p1_rewards, stats
 
 def action_counts(hand_list):
     fold_count = 0
@@ -338,7 +284,7 @@ def autolabel(rects, ax, percentages):
 def plot_action_analysis(wins, losses):
     win_fold_count, win_call_or_check_count = action_counts(wins)
     loss_fold_count, loss_call_or_check_count = action_counts(losses)
-    
+
     total_wins = len(wins)
     total_losses = len(losses)
 
@@ -378,44 +324,56 @@ def plot_action_analysis(wins, losses):
     fig.tight_layout()
     plt.show()
 
-def plot_statistics_overview(stats):
-    # Plotting Total Games, Win Rate, Draw Rate, Loss Rate, Average Win Margin, Average Loss Margin, Std Deviation
-    categories = ['Win Rate', 'Draw Rate', 'Loss Rate', 'Avg Win Margin', 'Avg Loss Margin', 'Std Deviation']
-    values = [stats['win_rate'] * 100, stats['draw_rate'] * 100, stats['loss_rate'] * 100,
-              stats['average_win_margin'], stats['average_loss_margin'], stats['std_deviation']]
+def main(directory_path, directory_path_2, directory_path_3):
+    all_matches_log1, wins, draws, losses, rewards, stats = analyze_directory(directory_path)
+    all_matches_log2, wins2, draws2, losses2, rewards2, stats2 = analyze_directory(directory_path_2)
+    all_matches_log3, wins3, draws3, losses3, rewards3, stats3 = analyze_directory(directory_path_3)
 
-    plt.figure(figsize=(10, 5))
-    bars = plt.bar(categories, values, color='skyblue')
 
-    plt.title('AI Performance Statistics')
-    plt.ylabel('Value')
-    plt.xticks(rotation=45, ha="right")
-
-    for bar in bars:
-        yval = bar.get_height()
-        plt.text(bar.get_x() + bar.get_width()/2, yval, round(yval, 2), va='bottom')  # Round to 2 decimals
-
-    plt.tight_layout()
-    plt.show()
-
-def main(directory_path):
-    wins, draws, losses, rewards, stats = analyze_directory(directory_path)
-    print("\nStatistics:")
+    print("\nStatistics for TCC_AI on seat 1 and DeepStack on seat 2:")
     for stat, value in stats.items():
         print(f"{stat.replace('_', ' ').title()}: {value}")
-    #plot_statistics(rewards, stats)
-    #plot_action_analysis(wins, losses)
-    #plot_statistics_overview(stats)
-    #plot_gaussian(rewards)
-    #plot_histogram(rewards)
-    
+
+    print("\nStatistics for DeepStack on seat 1 and TCC_AI on seat 2:")
+    for stat, value in stats2.items():
+        print(f"{stat.replace('_', ' ').title()}: {value}")
+
+    plot_statistics(rewards, rewards2, rewards3)
+
     all_matches = wins + draws + losses
     p1_should_win, p1_should_draw, p1_should_lose = get_hand_quality_proportion(all_matches)
 
+    print("\nlen(all_matches) old: ", len(all_matches))
     print("p1_should_win: ", len(p1_should_win)/len(all_matches))
     print("p1_should_draw: ", len(p1_should_draw)/len(all_matches))
     print("p1_should_lose: ", len(p1_should_lose)/len(all_matches))
 
+    # p1_should_win3, p1_should_draw3, p1_should_lose3 = get_hand_quality_proportion(all_matches_log1)
+
+    # print("\nlen(all_matches) log1: ", len(all_matches_log1))
+    # print("p1_should_win: ", len(p1_should_win3)/len(all_matches_log1))
+    # print("p1_should_draw: ", len(p1_should_draw3)/len(all_matches_log1))
+    # print("p1_should_lose: ", len(p1_should_lose3)/len(all_matches_log1))
+
+    all_matches2 = wins2 + draws2 + losses2
+    p1_should_win2, p1_should_draw2, p1_should_lose2 = get_hand_quality_proportion(all_matches2)
+
+    print("\nlen(all_matches) old: ", len(all_matches2))
+    print("p1_should_win: ", len(p1_should_win2)/len(all_matches2))
+    print("p1_should_draw: ", len(p1_should_draw2)/len(all_matches2))
+    print("p1_should_lose: ", len(p1_should_lose2)/len(all_matches2))
+
+    # p1_should_win4, p1_should_draw4, p1_should_lose4 = get_hand_quality_proportion(all_matches_log2)
+
+    # print("\nlen(all_matches) log2: ", len(all_matches_log2))
+    # print("p1_should_win: ", len(p1_should_win4)/len(all_matches_log2))
+    # print("p1_should_draw: ", len(p1_should_draw4)/len(all_matches_log2))
+    # print("p1_should_lose: ", len(p1_should_lose4)/len(all_matches_log2))
+
+    #plot_statistics(rewards, stats)
+    #plot_action_analysis(wins, losses)
+    #plot_gaussian(rewards)
+    #plot_histogram(rewards)
     # wins_folds_list, wins_checkcall_lists = action_lists(wins)
     # losses_folds_list, losses_checkcall_lists = action_lists(losses)
     # print("losses_folds_list:")
@@ -430,10 +388,19 @@ if __name__ == "__main__":
         sys.exit(1)
 
     log_folder_path = f'{directory_path}/../logs/matches/' + sys.argv[1]  # Get the directory path from the command-line argument
+    log_folder_path2 = f'{directory_path}/../logs/matches/' + sys.argv[2]  # Get the directory path from the command-line argument
+    log_folder_path3 = f'{directory_path}/../logs/matches/' + sys.argv[3]  # Get the directory path from the command-line argument
+
     if not os.path.isdir(log_folder_path):
         print(f"The provided directory does not exist: {sys.argv[1]}")
         sys.exit(1)
 
-    if len(sys.argv) == 3:
-        is_inverted_seat = sys.argv[2]
-    main(log_folder_path)
+    if not os.path.isdir(log_folder_path2):
+        print(f"The provided directory does not exist: {sys.argv[2]}")
+        sys.exit(1)
+    
+
+    if len(sys.argv) == 5:
+        is_inverted_seat = sys.argv[3]
+
+    main(log_folder_path, log_folder_path2, log_folder_path3)
