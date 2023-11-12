a3499b643ea76c9942d1cf874e4d46bd1773152f
Nelson Joppi <njoppi2@gmail.com>
2023-11-11 22:41:46 -0300

    add leduc poker gameplay to web interface

--- game_engine/common_types.py
+++ game_engine/common_types.py
@@ -4,11 +4,15 @@ import json
 
 PlayerGroups = Literal["all", "non_broke", "can_bet_in_current_turn", "active_in_hand", "all_in"]
 
-class Phases(Enum):
+class PokerPhases(Enum):
     PRE_FLOP = "PRE_FLOP"
     FLOP = "FLOP"
     TURN = "TURN"
     RIVER = "RIVER"
+
+class LeducPhases(Enum):
+    PRE_FLOP = "PRE_FLOP"
+    FLOP = "FLOP"
 
 class Encoder(json.JSONEncoder):
     def default(self, obj):
